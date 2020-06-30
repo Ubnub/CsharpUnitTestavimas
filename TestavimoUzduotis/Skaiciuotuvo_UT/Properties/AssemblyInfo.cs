76151f2129bf7742ce4a5892c8ec7b28e9a4d005
Ubnub <67596298+Ubnub@users.noreply.github.com>
2020-06-30 21:36:38 +0300

    Arvid
    Neatsirado, trying again

---
+++ TestavimoUzduotis/Skaiciuotuvo_UT/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Skaiciuotuvo_UT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Skaiciuotuvo_UT")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e114022-c1b6-40e9-895c-f154cb6adf95")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
